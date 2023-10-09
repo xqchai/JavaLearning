bb2ab5c157245902299506fa1ec50273a28ec389
xqchai <64688081+xqchai@users.noreply.github.com>
2023-10-09 21:24:05 +0800

    Add files via upload

--- Projects/JigsawPuzzle/src/Main.java
+++ Projects/JigsawPuzzle/src/Main.java
@@ -1,10 +1,18 @@
 import com.xqchai.ui.GameJFrame;
+import com.xqchai.ui.LoginJFrame;
+import com.xqchai.user.UserInformation;
 
 public class Main {
     /*程序主入口
     **/
     public static void main(String[] args) {
         //想开启谁的界面就创建谁的对象就可以了
-        new GameJFrame();
+
+        //加载数据列表
+        new UserInformation();
+        UserInformation.initUser();
+        //打开登录界面
+        new LoginJFrame();
+
     }
 }
